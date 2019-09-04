7c07900e22e95d0b1c5ba24d601361f5284f69ed
YanHao <yanhao@imdada.cn>
2019-09-04 16:56:26 +0800

    feat(index): add record page

--- src/app.tsx
+++ src/app.tsx
@@ -24,7 +24,8 @@ class App extends Component {
    */
   config: Config = {
     pages: [
-      'pages/index/index'
+      'pages/index/index',
+      'pages/record/record'
     ],
     window: {
       backgroundTextStyle: 'light',
