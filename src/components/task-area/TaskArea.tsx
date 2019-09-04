ad16e1acdf26659b75e1bec0daf50157e375c676
YanHao <yanhao@imdada.cn>
2019-09-04 16:56:12 +0800

    feat(task): mod task page

--- src/components/task-area/TaskArea.tsx
+++ src/components/task-area/TaskArea.tsx
@@ -1,5 +1,6 @@
 import Taro, { Component } from '@tarojs/taro'
 import { View } from '@tarojs/components'
+import { AtButton } from "taro-ui"
 import './TaskArea.scss'
 
 
@@ -15,13 +16,21 @@ export default class TaskArea extends Component {
 
     render () {
         const { tagName, tasks } = this.props
+
         return (
-            <View className='at-row'>
-                {
-                    tasks.map((task, idx) => (
-                        <View className='at-col'>{ task.name }</View>
-                    ))
-                }
+            <View className='task-group'>
+                <View className='task-tag'>{tagName}</View>
+                <View className='at-row at-row--wrap task-items'>
+                    {
+                        tasks.map((task, idx) => (
+                            <View className='at-col at-col-3'>
+                                <View className='task-item'>
+                                    <AtButton circle>{task.name}</AtButton>
+                                </View>
+                            </View>
+                        ))
+                    }
+                </View>
             </View>
         )
     }
