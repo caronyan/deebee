d74086c6de58f3197b15f6df6ce6c453058ca194
YanHao <yanhao@imdada.cn>
2019-09-05 18:11:58 +0800

    feat(index): mod page style & task component

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,10 +1,17 @@
 import Taro, { Component, Config } from '@tarojs/taro'
 import { View, Text } from '@tarojs/components'
+import { connect } from '@tarojs/redux'
+import { NavBar, BottomBar, TaskArea } from '@components'
+import { AtFab } from 'taro-ui'
+import * as taskActions from '@actions/task'
 import './index.scss'
 
-import { NavBar } from '@components'
-
+@connect(state => state.task, taskActions)
 export default class Index extends Component {
+
+  state = {
+    
+  }
 
   /**
    * 指定config的类型声明为: Taro.Config
@@ -19,7 +26,9 @@ export default class Index extends Component {
 
   componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount () {
+    this.props.getUserTasks(1)
+  }
 
   componentWillUnmount () { }
 
@@ -31,13 +40,31 @@ export default class Index extends Component {
 
   }
 
+  handleAddClick = () => {
+
+  }
+
   render () {
+    const { tasks } = this.props
     return (
       <View className='index'>
         <NavBar 
-          pageName='测试'
+          pageName='首页'
         />
-        <Text>Hello world!</Text>
+        {
+          tasks.map(tagGroup => (
+            <TaskArea 
+              tagName={tagGroup.tag}
+              tasks={tagGroup.list}
+            />
+          )) 
+        }
+        <View className='float-btn'>
+          <AtFab hover-class='fab-hover' onClick={this.handleAddClick.bind(this)}>
+            <Text className='at-fab__icon at-icon at-icon-add'></Text>
+          </AtFab>
+        </View>
+        <BottomBar />
       </View>
     )
   }
